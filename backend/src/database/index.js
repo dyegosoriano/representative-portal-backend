72b6c37d621d4a871c95147d15cf261aa1556e2c
Dyego Soriano <dyegodf2@gmail.com>
2020-06-14 11:53:07 -0300

    config Database

---
+++ backend/src/database/index.js
@@ -0,0 +1,26 @@
+import Sequelize from 'sequelize'
+
+import User from '../models/User'
+
+import databaseConfig from '../config/database'
+
+const models = [User]
+
+class Database {
+  constructor () {
+    this.init()
+  }
+
+  init () {
+    // Conectando com banco de dados
+    this.connection = new Sequelize(databaseConfig)
+
+    models
+    // Conectando models com banco de dados
+      .map((model) => model.init(this.connection))
+    // Chamando método de associação
+      .map(model => { model.associate && model.associate(this.connection.models) })
+  }
+}
+
+export default new Database()
