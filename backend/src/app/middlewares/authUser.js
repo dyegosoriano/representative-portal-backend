cef0e6d46decb981393c755915959470e383bac9
Dyego Soriano <dyegodf2@gmail.com>
2020-06-14 17:35:47 -0300

    add middleware authUser

---
+++ backend/src/app/middlewares/authUser.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+import { promisify } from 'util'
+
+import authConfig from '../../config/auth'
+
+export default async (request, response, next) => {
+  // Carregando token da requisição
+  const authHeader = request.headers.authorization
+
+  // Validando existência do token
+  if (!authHeader) { return response.status(401).json({ error: 'Token not provided' }) }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret)
+    request.userId = decoded.id
+    return next()
+  } catch (error) {
+    return response.status(401).json({ error: 'Token invalid' })
+  }
+}
