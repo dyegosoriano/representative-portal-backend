fafd56ef6b1065256d43adbee134918628ac7c44
Dyego Soriano <dyegodf2@gmail.com>
2020-06-14 13:47:22 -0300

    Authentication JWT

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 
 import UserController from './controllers/UserController'
+import SessionController from './controllers/SessionController'
 
 const routes = new Router()
 
 routes
   .post('/user', UserController.store)
+  .post('/sessions', SessionController.store)
 
 export default routes
