c80bd9360c48b3b170e81b6f0ae256d5101e5e88
Dyego Soriano <dyegodf2@gmail.com>
2020-06-18 16:56:07 -0300

    add seeds USERS

---
+++ backend/src/database/seeds/20200618193736-users.js
@@ -0,0 +1,44 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', [
+      {
+        name: 'Dyego Soriano',
+        email: 'dyego@email.com',
+        cpf: '12345678900',
+        password_hash: '$2a$08$F.bNRBusQ7IScngrclisieEYJXhZRPNl/qx/vnfNPLGixV1emUd22',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Rebecca Soriano',
+        email: 'rebecca@email.com',
+        cpf: '12345678901',
+        password_hash: '$2a$08$F.bNRBusQ7IScngrclisieEYJXhZRPNl/qx/vnfNPLGixV1emUd22',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Eliomar Assis',
+        email: 'eliomar@email.com',
+        cpf: '12345678903',
+        password_hash: '$2a$08$F.bNRBusQ7IScngrclisieEYJXhZRPNl/qx/vnfNPLGixV1emUd22',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: 'Valdirene Gonçalves',
+        email: 'lene@email.com',
+        cpf: '12345678904',
+        password_hash: '$2a$08$F.bNRBusQ7IScngrclisieEYJXhZRPNl/qx/vnfNPLGixV1emUd22',
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+    ], {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users', null, {})
+  }
+}
