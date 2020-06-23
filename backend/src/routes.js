e898a3e23e84aa26057f2b55d5ffcd0ed83841c6
Dyego Soriano <dyegodf2@gmail.com>
2020-06-23 16:20:04 -0300

    refactor Routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ import SessionController from './app/controllers/SessionController'
 import ProviderController from './app/controllers/ProviderController'
 import ProductController from './app/controllers/ProductController'
 import OrderController from './app/controllers/OrderController'
+import ItemController from './app/controllers/ItemController'
 
 import authUserMiddlewares from './app/middlewares/authUser'
 import authProviderMiddlewares from './app/middlewares/authProvider'
@@ -12,29 +13,35 @@ import authProviderMiddlewares from './app/middlewares/authProvider'
 const routes = new Router()
 
 routes
+  // Rotas de autenticação
+  .post('/sessions/user', SessionController.user)
+  .post('/sessions/provider', SessionController.provider)
+
+  // Rotas de usuários
   .post('/user', UserController.store)
-  .post('/provider', ProviderController.store)
-
-  .post('/user/sessions', SessionController.user)
-  .post('/provider/sessions', SessionController.provider)
-
   .get('/user', authUserMiddlewares, UserController.show)
   .put('/user', authUserMiddlewares, UserController.update)
 
-  .post('/user/order', authUserMiddlewares, OrderController.store)
-  .get('/user/order', authUserMiddlewares, OrderController.index)
-  .get('/user/order/:id', authUserMiddlewares, OrderController.show)
-  .delete('/user/order/:id', authUserMiddlewares, OrderController.delete)
-
-  .get('/provider', authProviderMiddlewares, ProviderController.show)
+  // Rotas de provedores
+  .post('/provider', ProviderController.store)
+  .get('/provider/:id', authProviderMiddlewares, ProviderController.show)
   .put('/provider', authProviderMiddlewares, ProviderController.update)
 
-  .post('/provider/products', authProviderMiddlewares, ProductController.store)
+  // Rotas de ordens de serviços
+  .post('/orders', authUserMiddlewares, OrderController.store)
+  .put('/orders/:id', authUserMiddlewares, OrderController.update)
+  .get('/orders', authUserMiddlewares, OrderController.index)
+  .get('/orders/:id', authUserMiddlewares, OrderController.show)
+  .delete('/orders/:id', authUserMiddlewares, OrderController.delete)
 
-  .get('/provider/products', authProviderMiddlewares, ProductController.index)
-  .get('/provider/products/:id', authProviderMiddlewares, ProductController.show)
+  // Rotas de item das rodens de serviços
+  .post('/user/order/:id/item', authUserMiddlewares, ItemController.store)
 
-  .put('/provider/products/:product_id', authProviderMiddlewares, ProductController.update)
-  .delete('/provider/products/:product_id', authProviderMiddlewares, ProductController.delete)
+  // Rotas de produtos
+  .post('/products', authProviderMiddlewares, ProductController.store)
+  .put('/products/:product_id', authProviderMiddlewares, ProductController.update)
+  .get('/products', authProviderMiddlewares, ProductController.index)
+  .get('/products/:id', authProviderMiddlewares, ProductController.show)
+  .delete('/products/:product_id', authProviderMiddlewares, ProductController.delete)
 
 export default routes
