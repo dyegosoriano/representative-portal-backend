aefaf16ba4b9f38c1179618e84c78159b441a8dc
Dyego Soriano <dyegodf2@gmail.com>
2020-07-07 15:39:17 -0300

    Refactoring Login page styles

--- mobile/src/pages/Login/styles.js
+++ mobile/src/pages/Login/styles.js
@@ -3,11 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
-
-  padding-top: 32px;
-  padding-bottom: 32px;
-  padding-right: 32px;
-  padding-left: 32px;
+  padding: 30px;
 `;
 
 export const Welcome = styled.Text`
@@ -27,8 +23,7 @@ export const UserInput = styled.TextInput`
   border-radius: 10px;
 
   margin-bottom: 25px;
-  padding-left: 24px;
-  padding-right: 24px;
+  padding: 0 20px;
 `;
 
 export const PasswordInput = styled.TextInput`
@@ -38,8 +33,7 @@ export const PasswordInput = styled.TextInput`
   border-radius: 10px;
 
   margin-bottom: 25px;
-  padding-left: 24px;
-  padding-right: 24px;
+  padding: 0 20px;
 `;
 
 export const Button = styled.TouchableOpacity`
