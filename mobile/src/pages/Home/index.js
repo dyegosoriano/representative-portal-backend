5ebb6b4da88dac8a59510a0950d4c82491513fb0
Dyego Soriano <dyegodf2@gmail.com>
2020-07-09 14:22:13 -0300

    edit Home page

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Feather as Icon } from '@expo/vector-icons';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserCnpj,
+  UserName,
+  Footer,
+  Button,
+  ButtonText,
+  ButtonIcon,
+} from './styles';
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  function handleNavigate(destiny) {
+    navigation.navigate(destiny);
+  }
+
   return (
     <Container>
-      <Title>Home</Title>
+      <Header>
+        <UserName>Dyego Soriano</UserName>
+        <UserCnpj>CNPJ: 1232131241343</UserCnpj>
+      </Header>
+
+      <Footer>
+        <Button onPress={() => handleNavigate('NewOrders')}>
+          <ButtonText>Novo pedido</ButtonText>
+          <ButtonIcon>
+            <Icon name="plus-square" color="#fff" size={24} />
+          </ButtonIcon>
+        </Button>
+
+        <Button onPress={() => handleNavigate('Orders')}>
+          <ButtonText>Meus pedidos</ButtonText>
+          <ButtonIcon>
+            <Icon name="shopping-cart" color="#fff" size={24} />
+          </ButtonIcon>
+        </Button>
+
+        <Button onPress={() => handleNavigate('Profile')}>
+          <ButtonText>Meu perfil</ButtonText>
+          <ButtonIcon>
+            <Icon name="user" color="#fff" size={24} />
+          </ButtonIcon>
+        </Button>
+      </Footer>
     </Container>
   );
 };
--- mobile/src/pages/Home/styles.js
+++ mobile/src/pages/Home/styles.js
@@ -2,8 +2,55 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  padding: 10px;
+  justify-content: space-between;
 `;
 
-export const Title = styled.Text``;
+export const Header = styled.View`
+  height: 100px;
+  padding: 0 30px;
+  background: #fff;
+  justify-content: center;
+`;
+
+export const UserName = styled.Text`
+  margin-top: 20px;
+  font-size: 36px;
+  text-align: right;
+  color: #222;
+`;
+
+export const UserCnpj = styled.Text`
+  text-align: right;
+  font-style: italic;
+  font-size: 14px;
+  color: #222;
+`;
+
+export const Footer = styled.View`
+  margin: 0 30px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background: #00bfa5;
+  height: 50px;
+  border-radius: 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
+
+export const ButtonText = styled.Text`
+  flex: 1;
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  margin-left: 60px;
+`;
+
+export const ButtonIcon = styled.View`
+  width: 60px;
+  height: 60px;
+  align-items: center;
+  justify-content: center;
+`;
