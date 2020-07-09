8b46aaeb674b6ea231c1416678d08deff8ea7fdf
Dyego Soriano <dyegodf2@gmail.com>
2020-07-09 14:45:53 -0300

    adicionando função de voltar

--- mobile/src/pages/NewOrders/index.js
+++ mobile/src/pages/NewOrders/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 import { Container, Header, ExitButton, Title } from './styles';
 
 const Orders = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
   return (
     <Container>
       <Header>
-        <ExitButton>
+        <ExitButton onPress={handleNavigateBack}>
           <Icon name="arrow-left" color="#00bfa5" size={24} />
         </ExitButton>
 
