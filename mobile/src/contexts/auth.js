a1e8f439808b227bf9e1aa90b5602f1bec587699
Dyego Soriano <dyegodf2@gmail.com>
2020-07-27 14:38:40 -0300

    refatorando AuthContext

--- mobile/src/contexts/auth.js
+++ mobile/src/contexts/auth.js
@@ -1,10 +1,27 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
-const AuthContext = createContext({});
+import * as auth from '../services/auth';
+
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  async function handleSignIn(email, password) {
+    const response = await auth.signIn(email, password);
+
+    setUser(response.user);
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: false, token: '', user: {} }}>
+    <AuthContext.Provider
+      value={{
+        signed: Boolean(user),
+        token: '',
+        user,
+        handleSignIn,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
