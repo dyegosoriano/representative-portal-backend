bf194f99c4efa58874f0b98aee0b708bdd035be0
Dyego Soriano <dyegodf2@gmail.com>
2020-08-06 20:23:24 -0300

    variáveis de ambiente

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -1,5 +1,7 @@
+import 'dotenv/config'
+
 export default {
-  secretUser: 'userSecret',
-  secretProvider: 'providerSrcret',
-  expiresIn: '7d'
+  secretUser: process.env.USER_SECRET,
+  secretProvider: process.env.PROVIDER_SECRET,
+  expiresIn: process.env.DAY_EXPIRES
 }
