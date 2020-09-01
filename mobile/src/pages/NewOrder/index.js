c31316d4a380b4c72aee8a67853c4b8f8ccad20f
Dyego Soriano <dyegodf2@gmail.com>
2020-09-01 19:15:23 -0300

    add Modal e Picker na screen NewOrder

--- mobile/src/pages/NewOrder/index.js
+++ mobile/src/pages/NewOrder/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
-import { Alert, Button } from 'react-native';
+import { Alert, Button, Modal, Text, TextInput, View } from 'react-native';
 
-import { Container, OptionsBox, Title } from './styles';
+import {
+  Container,
+  BoxModal,
+  BoxAmount,
+  BoxModalText,
+  OptionsBox,
+  Picker,
+  Strong,
+  Title,
+} from './styles';
 
 const NewOrder = () => {
   const navigation = useNavigation();
+
+  const [selectedValue, setSelectedValue] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
 
   async function confirmOrder() {
     Alert.alert('Aviso', 'Deseja concluir o pedido?', [
@@ -24,11 +36,69 @@ const NewOrder = () => {
 
   return (
     <Container>
-      <Title>Pedido numero: 231</Title>
+      <Title>
+        Pedido: <Strong>231</Strong>
+      </Title>
+
+      <Modal animationType="slide" transparent visible={modalVisible}>
+        <BoxModal>
+          <View>
+            <BoxModalText>
+              Produto selecionado: <Strong>{selectedValue}</Strong>
+            </BoxModalText>
+            <BoxModalText>
+              Valor unitário: <Strong>R$ 15</Strong>
+            </BoxModalText>
+            <BoxModalText>
+              Valor total: <Strong>R$ 15</Strong>
+            </BoxModalText>
+
+            <BoxAmount>
+              <Text>Quantidade: </Text>
+              <TextInput
+                placeholder="Insira o valor desejado"
+                keyboardType="number-pad"
+                returnKeyType="done"
+              />
+            </BoxAmount>
+          </View>
+
+          <Picker
+            selectedValue={selectedValue}
+            onValueChange={(value, index) => setSelectedValue(value)}
+          >
+            <Picker.Item label="Produto 1" value="Produto 1" />
+            <Picker.Item label="Produto 2" value="Produto 2" />
+            <Picker.Item label="Produto 3" value="Produto 3" />
+            <Picker.Item label="Produto 4" value="Produto 4" />
+            <Picker.Item label="Produto 5" value="Produto 5" />
+            <Picker.Item label="Produto 6" value="Produto 6" />
+            <Picker.Item label="Produto 7" value="Produto 7" />
+            <Picker.Item label="Produto 8" value="Produto 8" />
+            <Picker.Item label="Produto 9" value="Produto 9" />
+            <Picker.Item label="Produto 10" value="Produto 10" />
+            <Picker.Item label="Produto 11" value="Produto 11" />
+            <Picker.Item label="Produto 12" value="Produto 12" />
+            <Picker.Item label="Produto 13" value="Produto 13" />
+            <Picker.Item label="Produto 14" value="Produto 14" />
+            <Picker.Item label="Produto 15" value="Produto 15" />
+            <Picker.Item label="Produto 16" value="Produto 16" />
+            <Picker.Item label="Produto 17" value="Produto 17" />
+            <Picker.Item label="Produto 18" value="Produto 18" />
+            <Picker.Item label="Produto 19" value="Produto 19" />
+            <Picker.Item label="Produto 20" value="Produto 20" />
+          </Picker>
+
+          <View>
+            <Button title="Adicionar" onPress={() => setModalVisible(false)} />
+            <Button title="Cancelar" onPress={() => setModalVisible(false)} />
+          </View>
+        </BoxModal>
+      </Modal>
 
       <OptionsBox>
         <Button title="Confirmar" onPress={() => confirmOrder()} />
-        <Button title="Novo item" />
+        <Button title="Novo item" onPress={() => setModalVisible(true)} />
         <Button title="Cancelar" onPress={() => cancelOrder()} />
       </OptionsBox>
     </Container>
--- mobile/src/pages/NewOrder/styles.js
+++ mobile/src/pages/NewOrder/styles.js
@@ -10,10 +10,38 @@ export const Title = styled.Text`
   margin-top: 20px;
 `;
 
+export const Strong = styled.Text`
+  font-weight: bold;
+`;
+
+export const BoxModal = styled.View`
+  background: white;
+  padding: 100px 20px 20px 20px;
+
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const BoxModalText = styled.Text`
+  font-size: 16px;
+  margin-bottom: 8px;
+`;
+
+export const BoxAmount = styled.View`
+  flex-direction: row;
+  width: 300px;
+`;
+
+export const Picker = styled.Picker`
+  width: 100%;
+`;
+
 export const OptionsBox = styled.View`
   width: 100%;
   padding: 20px 0px;
 
   flex-direction: row;
+  align-items: center;
   justify-content: space-around;
 `;
