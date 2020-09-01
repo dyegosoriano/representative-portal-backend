1255e112d21a3e05089b032a249c7d7e8a859a27
Dyego Soriano <dyegodf2@gmail.com>
2020-09-01 17:14:44 -0300

    adicionando alertas ao NewOrder

--- mobile/src/pages/NewOrder/index.js
+++ mobile/src/pages/NewOrder/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { Button } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import { Alert, Button } from 'react-native';
 
 import { Container, OptionsBox, Title } from './styles';
 
 const NewOrder = () => {
+  const navigation = useNavigation();
+
+  async function confirmOrder() {
+    Alert.alert('Aviso', 'Deseja concluir o pedido?', [
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+      { text: 'Cancelar' },
+    ]);
+  }
+
+  async function cancelOrder() {
+    Alert.alert('Aviso', 'Deseja cancelar o pedido?', [
+      { text: 'OK', onPress: () => navigation.goBack() },
+      { text: 'Cancelar' },
+    ]);
+  }
+
   return (
     <Container>
       <Title>Pedido numero: 231</Title>
 
       <OptionsBox>
-        <Button title="Confirmar" />
+        <Button title="Confirmar" onPress={() => confirmOrder()} />
         <Button title="Novo item" />
-        <Button title="Cancelar" />
+        <Button title="Cancelar" onPress={() => cancelOrder()} />
       </OptionsBox>
     </Container>
   );
