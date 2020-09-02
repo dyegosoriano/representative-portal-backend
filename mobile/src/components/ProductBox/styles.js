277c494359a14d513cf70c0c7efe7a5b6a44d6f4
Dyego Soriano <dyegodf2@gmail.com>
2020-09-02 02:21:15 -0300

    componente ProductBox

---
+++ mobile/src/components/ProductBox/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding: 16px;
+  margin-bottom: 16px;
+  background: #fff;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
