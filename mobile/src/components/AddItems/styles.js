c6c8bc74ce4b84719538a09c69ebcd5c36294f93
Dyego Soriano <dyegodf2@gmail.com>
2020-09-10 19:32:26 -0300

    criação do componente AddItems

---
+++ mobile/src/components/AddItems/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native'
+
+export const BoxModal = styled.View`
+  background: white;
+  padding: 100px 16px 16px 16px;
+
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const BoxModalText = styled.Text`
+  font-size: 16px;
+  margin-bottom: 8px;
+`
+
+export const BoxAmount = styled.View`
+  flex-direction: row;
+  width: 300px;
+`
+
+export const Picker = styled.Picker`
+  width: 100%;
+`
+
+export const Strong = styled.Text`
+  font-weight: bold;
+`
