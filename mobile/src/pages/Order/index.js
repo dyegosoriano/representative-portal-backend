6d2cf9367b926e3307e8cf7d0697426700467deb
Dyego Soriano <dyegodf2@gmail.com>
2020-09-10 15:07:51 -0300

    refatorando Order

--- mobile/src/pages/Order/index.js
+++ mobile/src/pages/Order/index.js
@@ -1,89 +1,118 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import { Alert, Button, View } from 'react-native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 
-import { View, TouchableOpacity } from 'react-native';
-import { useRoute, useNavigation } from '@react-navigation/native';
-import { Feather as Icon } from '@expo/vector-icons';
+import api from '../../services/api'
+
+import ProductBox from '../../components/ProductBox'
 
 import {
-  ProductsList,
-  Product,
+  Container,
+  Footer,
   ProductName,
   ProductText,
-  ProductButtons,
-  Footer,
-  ButtonText,
-  Button,
-} from './styles';
-
-import api from '../../services/api';
+  ScrollProducts,
+  Strong,
+  Title,
+} from './styles'
 
 const Order = () => {
-  const [products, setProducts] = useState([]);
+  const [productsOrder, setProductsOrder] = useState([])
 
-  const route = useRoute();
+  const navigation = useNavigation()
+  const route = useRoute()
 
-  const { order } = route.params;
-  const navigation = useNavigation();
+  const { order } = route.params
 
   async function getItens() {
-    const response = await api.get(`/orders/${order.id}`);
-    setProducts(response.data.itens);
-  }
+    try {
+      const response = await api.get(`/orders/${order.id}`)
 
-  async function confirmOrder() {
-    await api.put(`/orders/${order.id}`, { closed: true });
-    navigation.goBack();
-  }
-
-  async function deleteOrder() {
-    await api.delete(`/orders/${order.id}`);
-    navigation.goBack();
+      setProductsOrder(response.data.itens)
+    } catch (error) {
+      console.log(`error.message >>> ${error.message} <<<`)
+    }
   }
 
   async function deleteItem(id) {
-    await api.delete(`/items/${id}`);
-    getItens();
+    try {
+      await api.delete(`/items/${id}`)
+
+      getItens()
+    } catch (error) {
+      console.log(`error.message >>> ${error.message} <<<`)
+    }
+  }
+
+  async function confirmOrder() {
+    Alert.alert('Aviso', 'Deseja concluir o pedido?', [
+      {
+        text: 'OK',
+        onPress: async () => {
+          await api.put(`/orders/${order.id}`, { closed: true })
+
+          navigation.goBack()
+        },
+      },
+      { text: 'Cancelar' },
+    ])
+  }
+
+  async function cancelOrder() {
+    Alert.alert('Aviso', 'Deseja cancelar o pedido?', [
+      {
+        text: 'OK',
+        onPress: async () => {
+          await api.delete(`/orders/${order.id}`)
+
+          navigation.goBack()
+        },
+      },
+      { text: 'Cancelar' },
+    ])
   }
 
   useEffect(() => {
-    getItens();
-  }, []);
+    getItens()
+  }, [])
 
   return (
-    <>
-      <ProductsList>
-        {products.map((product) => (
-          <Product key={product.id}>
+    <Container>
+      <Title>
+        Pedido: <Strong>{order.id}</Strong>
+      </Title>
+
+      <ScrollProducts
+        data={productsOrder}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <ProductBox>
             <View>
-              <ProductName>Nome do produto</ProductName>
-              <ProductText>Quantidade: {product.amount}</ProductText>
-              <ProductText>R$: {product.total_price}</ProductText>
+              <ProductName>Chocolate</ProductName>
+              <ProductText>
+                Quantidade: <Strong>{item.amount}</Strong>
+              </ProductText>
+              <ProductText>
+                valor total: <Strong>R$ {item.total_price}</Strong>
+              </ProductText>
             </View>
 
-            <ProductButtons>
-              <Icon name="edit" size={24} color="#fff" />
-
-              <TouchableOpacity onPress={() => deleteItem(product.id)}>
-                <Icon name="trash-2" size={24} color="#fff" />
-              </TouchableOpacity>
-            </ProductButtons>
-          </Product>
-        ))}
-      </ProductsList>
+            <Button
+              title="Excluir"
+              color="#f00"
+              onPress={() => deleteItem(item.id)}
+            />
+          </ProductBox>
+        )}
+      />
 
       <Footer>
-        <Button onPress={() => confirmOrder()}>
-          <ButtonText>Concluir </ButtonText>
-          <Icon name="check" size={24} color="#fff" />
-        </Button>
-
-        <Button onPress={() => deleteOrder()} color="#ff1744">
-          <ButtonText>Excluir </ButtonText>
-          <Icon name="trash-2" size={24} color="#fff" />
-        </Button>
+        <Button title="Confirmar" onPress={() => confirmOrder()} />
+        {/* <Button title="Novo item" onPress={() => setModalVisible(true)} /> */}
+        <Button title="Cancelar" onPress={() => cancelOrder()} color="#f00" />
       </Footer>
-    </>
-  );
-};
+    </Container>
+  )
+}
 
-export default Order;
+export default Order
--- mobile/src/pages/Order/styles.js
+++ mobile/src/pages/Order/styles.js
@@ -1,53 +1,42 @@
-import styled from 'styled-components/native';
+import styled from 'styled-components/native'
 
-export const ProductsList = styled.ScrollView`
-  padding: 0px 20px;
-`;
-
-export const Product = styled.View`
-  padding: 20px;
-  margin-top: 20px;
-  background: #607d8b;
-  border-radius: 5px;
-  flex-direction: row;
-  justify-content: space-between;
+export const Container = styled.View`
+  flex: 1;
   align-items: center;
-`;
+`
+
+export const Strong = styled.Text`
+  font-weight: bold;
+`
+
+export const Title = styled.Text`
+  padding: 16px 0px;
+  font-size: 16px;
+`
+
+export const ScrollProducts = styled.FlatList`
+  flex: 1;
+  width: 100%;
+  padding: 0 16px;
+`
 
 export const ProductName = styled.Text`
-  color: #fff;
-  font-size: 30px;
   margin-bottom: 10px;
-`;
+  font-weight: bold;
+  font-size: 16px;
+  color: #000;
+`
 
 export const ProductText = styled.Text`
-  color: #fff;
-  font-size: 20px;
-`;
-
-export const ProductButtons = styled.View`
-  width: 75px;
-  flex-direction: row;
-  justify-content: space-between;
-`;
+  font-size: 16px;
+  color: #000;
+`
 
 export const Footer = styled.View`
-  height: 100px;
-  background: #fff;
+  width: 100%;
+  padding: 16px 0px;
+
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
-`;
-
-export const ButtonText = styled.Text`
-  font-size: 22px;
-  color: #fff;
-`;
-
-export const Button = styled.TouchableOpacity`
-  background: ${(props) => (props.color ? props.color : '#212121')};
-  flex-direction: row;
-  align-items: center;
-  padding: 10px;
-  border-radius: 5px;
-`;
+`
