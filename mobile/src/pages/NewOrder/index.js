1e5ade45afc839883c4a41f4692839d5c9e71dd7
Dyego Soriano <dyegodf2@gmail.com>
2020-09-10 15:09:32 -0300

    refatorando NewOrder

--- mobile/src/pages/NewOrder/index.js
+++ mobile/src/pages/NewOrder/index.js
@@ -1,42 +1,38 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect } from 'react'
 import { Alert } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 import api from '../../services/api'
 
 import Loading from '../Loading'
-import ItemsList from '../../components/ItemsList'
 
 const NewOrder = () => {
-  const [order, setOrder] = useState({})
-  const [loading, setLoading] = useState(true)
-
   const navigation = useNavigation()
 
-  useEffect(() => {
-    async function getNewOrder() {
-      try {
-        const response = await api.post('/orders')
+  async function getNewOrder() {
+    try {
+      const response = await api.post('/orders')
 
-        setOrder(response.data)
-        setLoading(false)
-      } catch (error) {
-        console.log(`error.message >>> ${error.message} <<<`)
+      const order = response.data
 
-        Alert.alert('Alerta', 'Houve um problema com a conexão', [
-          {
-            text: 'Ok',
-            onPress: () => navigation.goBack(),
-          },
-        ])
-      }
+      navigation.navigate('Order', { order })
+    } catch (error) {
+      console.log(`error.message >>> ${error.message} <<<`)
+
+      Alert.alert('Alerta', 'Houve um problema com a conexão', [
+        {
+          text: 'Ok',
+          onPress: () => navigation.goBack(),
+        },
+      ])
     }
+  }
 
+  useEffect(() => {
     getNewOrder()
   }, [])
 
-  return loading ? <Loading /> : <ItemsList order={order} />
+  return <Loading />
 }
 
 export default NewOrder
