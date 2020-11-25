a9be33af2c4cb50fc4035fbbc6b21fee95ee78a4
Dyego Soriano <dyegodf2@gmail.com>
2020-11-25 16:28:07 -0300

    inicializando a conexão do typeorm com banco de dados

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist'
+
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  migrations: [`${rootDir}/database/migrations/*.{js,ts}`],
+  entities: [`${rootDir}/app/models/*.{js,ts}`],
+  synchronize: false,
+  logging: false,
+  cli: {
+    migrationsDir: ['src/database/migrations/'],
+    entitiesDir: ['src/app/models/'],
+  },
+}
