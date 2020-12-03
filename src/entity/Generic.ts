de804e80ca9ed7eef979a6f95f61ff5338b696a4
Dyego Soriano <dyegodf2@gmail.com>
2020-12-03 13:09:29 -0300

    model genérica

---
+++ src/entity/Generic.ts
@@ -0,0 +1,16 @@
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+export default abstract class Generic {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @CreateDateColumn({ type: 'timestamp with time zone', name: 'created_at' })
+  created_at: Date
+
+  @UpdateDateColumn({ type: 'timestamp with time zone', name: 'updated_at' })
+  updated_at: Date
+}
