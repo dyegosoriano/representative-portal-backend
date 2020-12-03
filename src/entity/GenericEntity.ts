a3d4eabe3967db14fe1eec7cddc0f28fc200d6e3
Dyego Soriano <dyegodf2@gmail.com>
2020-12-03 18:43:52 -0300

    criação de entidade genérica

---
+++ src/entity/GenericEntity.ts
@@ -0,0 +1,17 @@
+import { Column } from 'typeorm'
+
+import Generic from './Generic'
+
+export default abstract class GenericEntity extends Generic {
+  @Column('character varying', { name: 'name', length: 255 })
+  name: string
+
+  @Column('character varying', { name: 'password', length: 255 })
+  password: string
+
+  @Column('character varying', { name: 'email', unique: true, length: 255 })
+  email: string
+
+  @Column('character varying', { name: 'cnpj', unique: true, length: 255 })
+  cnpj: string
+}
