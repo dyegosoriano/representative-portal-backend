b8f0dae83fc608bf1b50e0a07212795e47357bea
Dyego Soriano <dyegodf2@gmail.com>
2020-12-03 19:23:03 -0300

    criação da service CreateUserService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,42 @@
+import { getRepository } from 'typeorm'
+
+import User from '@entity/User'
+
+import users_view, { UserRender } from '@views/users_view'
+import { passwordEncrypt } from '@util/password'
+import AppError from '@errors/AppError'
+
+interface Request {
+  password: string
+  email: string
+  name: string
+  cnpj: number
+}
+
+export default class CreateUserService {
+  async execute({ name, email, cnpj, password }: Request): Promise<UserRender> {
+    const userRepository = getRepository(User)
+    const userExist = await userRepository.find({
+      where: [{ email }, { cnpj }],
+    })
+
+    userExist.find(user => {
+      if (user.email === email)
+        throw new AppError('Este email ja foi cadastrado!', 401)
+      if (user.cnpj === cnpj)
+        throw new AppError('Este CNPJ ja foi cadastrado!', 401)
+    })
+
+    const data = {
+      password: await passwordEncrypt(password),
+      email,
+      name,
+      cnpj,
+    }
+
+    const user = userRepository.create(data)
+    const userCreated = await userRepository.save(user)
+
+    return users_view.render(userCreated)
+  }
+}
