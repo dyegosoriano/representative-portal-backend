92d954b5f881aa68c4853bc281fc7bfbc3126b4e
Dyego Soriano <dyegodf2@gmail.com>
2020-12-03 17:26:21 -0300

    refatorando rota de autenticação

--- src/routes/route.sessions.ts
+++ src/routes/route.sessions.ts
@@ -1,56 +1,32 @@
 import { Request, Response, Router } from 'express'
-import { getRepository } from 'typeorm'
-import jwt from 'jsonwebtoken'
 
-import { passwordCheck } from '@util/password'
-import AppError from '@errors/AppError'
-import authConfig from '@config/auth'
-
-import Providers from '@entity/Providers'
-import Users from '@entity/Users'
+import AuthenticateProviderService from '@services/AuthenticateProviderService'
+import AuthenticateUserService from '@services/AuthenticateUserService'
 
 const sessionsRoute = Router()
 
 sessionsRoute.post('/user', async (req: Request, res: Response) => {
   const { email, password } = req.body
 
-  const userRepository = getRepository(Users)
-  const user = await userRepository.findOneOrFail({ where: { email } })
-
-  if (!(await passwordCheck(password, user.password_hash))) {
-    throw new AppError('password does not match', 401)
-  }
-
-  const { id, name, cnpj } = user
-
-  return res.json({
-    user: { name, email, cnpj },
-    token: jwt.sign({ id }, authConfig.secretUser, {
-      expiresIn: authConfig.expiresIn,
-    }),
+  const authenticateUser = new AuthenticateUserService()
+  const responseAuthenticate = await authenticateUser.execute({
+    email,
+    password,
   })
+
+  return res.json(responseAuthenticate)
 })
 
 sessionsRoute.post('/provider', async (req: Request, res: Response) => {
   const { email, password } = req.body
 
-  const providerRepository = getRepository(Providers)
-  const provider = await providerRepository.findOneOrFail({
-    where: { email },
+  const authenticateProvider = new AuthenticateProviderService()
+  const responseAuthenticate = await authenticateProvider.execute({
+    email,
+    password,
   })
 
-  if (!(await passwordCheck(password, provider.password_hash))) {
-    throw new AppError('password does not match', 401)
-  }
-
-  const { id, name_provider, cnpj } = provider
-
-  return res.json({
-    provider: { name_provider, email, cnpj },
-    token: jwt.sign({ id }, authConfig.secretProvider, {
-      expiresIn: authConfig.expiresIn,
-    }),
-  })
+  return res.json(responseAuthenticate)
 })
 
 export default sessionsRoute
