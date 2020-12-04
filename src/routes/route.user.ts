fadd035ec17a15b4800f60101333e0a4cc4dbcc0
Dyego Soriano <dyegodf2@gmail.com>
2020-12-04 07:46:04 -0300

    refatorando rotas de usuários

--- src/routes/route.user.ts
+++ src/routes/route.user.ts
@@ -1,57 +1,40 @@
-import { Request, Response, Router } from 'express'
-import { getRepository } from 'typeorm'
+import { Router } from 'express'
 
-import { passwordCheck, passwordEncrypt } from '@util/password'
+import CreateUserService from '@services/CreateUserService'
+import UpdateUserService from '@services/UpdateUserService'
+import ShowUserService from '@services/ShowUserService'
+
 import authUserMiddleware from '@middleware/authUser'
-import users_view from '@views/users_view'
-import AppError from '@errors/AppError'
-
-import Users from '@entity/Users'
 
 const userRoute = Router()
 
-userRoute.post('/', async (req: Request, res: Response) => {
-  const { name, email, cnpj, password } = req.body
+userRoute.post('/', async (request, response) => {
+  const { name, email, cnpj, password } = request.body
 
-  const userRepository = getRepository(Users)
-  const userExist = await userRepository.find({
-    where: [{ email }, { cnpj }],
-  })
-
-  userExist.find(user => {
-    if (user.email === email)
-      throw new AppError('The email already exists!', 401)
-    if (user.cnpj === cnpj) throw new AppError('The CNPJ already exists!', 401)
-  })
-
-  const data = {
-    password_hash: await passwordEncrypt(password),
-    created_at: new Date(),
-    updated_at: new Date(),
+  const createUser = new CreateUserService()
+  const user = await createUser.execute({
+    password,
     email,
-    name,
     cnpj,
-  }
+    name,
+  })
 
-  const user = userRepository.create(data)
-  const userCreated = await userRepository.save(user)
-
-  return res.json(users_view.render(userCreated))
+  return response.json(user)
 })
 
 userRoute.use(authUserMiddleware)
 
-userRoute.get('/', async (req: Request, res: Response) => {
-  const id = req.userId
+userRoute.get('/', async (request, response) => {
+  const { id } = request.user
 
-  const userRepo = getRepository(Users)
-  const user = await userRepo.findOneOrFail({ id })
+  const showUser = new ShowUserService()
+  const user = await showUser.execute({ id })
 
-  return res.json(users_view.render(user))
+  return response.json(user)
 })
 
-userRoute.put('/', async (req: Request, res: Response) => {
-  const id = req.userId
+userRoute.put('/', async (request, response) => {
+  const { id } = request.user
   const {
     confirmationPassword,
     oldPassword,
@@ -59,53 +42,20 @@ userRoute.put('/', async (req: Request, res: Response) => {
     name,
     email,
     cnpj,
-  } = req.body
+  } = request.body
 
-  const userRepository = getRepository(Users)
-  const user = await userRepository.findOneOrFail({ id })
+  const updateUser = new UpdateUserService()
+  const user = await updateUser.execute({
+    confirmationPassword,
+    newPassword,
+    oldPassword,
+    email,
+    name,
+    cnpj,
+    id,
+  })
 
-  const passwordChecked = await passwordCheck(oldPassword, user.password_hash)
-
-  if (oldPassword && !passwordChecked) {
-    throw new AppError('Password does not match', 401)
-  }
-
-  if (newPassword === confirmationPassword) {
-    user.password_hash = await passwordEncrypt(newPassword)
-  } else {
-    throw new AppError(
-      'The confirmation password does not match the new password',
-      401,
-    )
-  }
-
-  if (email || cnpj) {
-    const userExist = await userRepository.find({
-      where: [{ email }, { cnpj }],
-    })
-
-    userExist.find(item => {
-      if (user.id !== item.id) {
-        if (item.email === email) {
-          throw new AppError('The email already exists!', 401)
-        }
-        if (item.cnpj === cnpj) {
-          throw new AppError('The CNPJ already exists!', 401)
-        }
-      }
-    })
-
-    user.email !== email ? (user.email = email) : false
-    user.cnpj !== cnpj ? (user.cnpj = cnpj) : false
-  }
-
-  if (name) {
-    user.name = name
-  }
-
-  await userRepository.save(user)
-
-  return res.json(users_view.render(user))
+  return response.json(user)
 })
 
 export default userRoute
