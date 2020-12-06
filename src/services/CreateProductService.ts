574d29b4b84d3bf85d5f7f061f33baaa5f70c114
Dyego Soriano <dyegodf2@gmail.com>
2020-12-05 23:10:52 -0300

    criação da service CreateProductService

---
+++ src/services/CreateProductService.ts
@@ -0,0 +1,29 @@
+import { getRepository } from 'typeorm'
+
+import products_view, { ProductRender } from '@views/products_view'
+
+import Product from '@entity/Product'
+
+interface Request {
+  provider_id: any
+  product: string
+  amount: number
+  price: number
+}
+
+export default class CreateProductService {
+  async execute({ provider_id, product, amount, price }: Request): Promise<ProductRender> {
+    const productRepository = getRepository(Product)
+
+    const newProduct = productRepository.create({
+      provider_id,
+      product,
+      amount,
+      price,
+    })
+
+    const productCreated = await productRepository.save(newProduct)
+
+    return products_view.render(productCreated)
+  }
+}
