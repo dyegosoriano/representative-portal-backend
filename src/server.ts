fb78596fd6aa0f3337ff9218b098e2fe7e03c795
Dyego Soriano <dyegodf2@gmail.com>
2020-12-07 19:22:31 -0300

    refatorando server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,26 @@
+import express from 'express'
 import dotenv from 'dotenv'
-import app from './app'
+import cors from 'cors'
+
+import 'express-async-errors'
+import 'reflect-metadata'
+import './database'
+
+import errorHandling from '@middleware/errorHandling'
+import logRequest from '@middleware/logRequest'
+import routes from './routes'
 
 dotenv.config()
 
+const app = express()
 const port = process.env.PORT || 3333
+
+app.use(cors())
+app.use(express.json())
+app.use(logRequest)
+app.use(routes)
+
+app.use(errorHandling.notFound)
+app.use(errorHandling.globalErrors)
 
 app.listen(port, () => console.log(`🚀  Server is running port: ${port}`))
