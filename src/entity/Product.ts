1dfe88dbe03341f83f10e9df01ac9b67d9cf3ab7
Dyego Soriano <dyegodf2@gmail.com>
2020-12-07 22:14:34 -0300

    criando relação entre Provider e Product

---
+++ src/entity/Product.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm'
+
+import Provider from './Provider'
+import Generic from './Generic'
+
+@Entity('products')
+export default class Product extends Generic {
+  @Column('character varying', { name: 'product', length: 255 })
+  product: string
+
+  @Column('numeric', { name: 'price' })
+  price: number
+
+  @Column('integer', { name: 'amount' })
+  amount: number
+
+  @ManyToOne(() => Provider, provider => provider.products, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'provider', referencedColumnName: 'id' })
+  provider: Provider
+}
--- src/entity/Provider.ts
+++ src/entity/Provider.ts
@@ -1,10 +1,14 @@
 import { Entity, OneToMany } from 'typeorm'
 
 import GenericEntity from './GenericEntity'
+import Product from './Product'
 import User from './User'
 
 @Entity('providers')
 export default class Provider extends GenericEntity {
   @OneToMany(() => User, user => user.id)
   users: User[]
+
+  @OneToMany(() => Product, product => product.id)
+  products: Product[]
 }
