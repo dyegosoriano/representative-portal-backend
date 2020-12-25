e0d729de27be0e55d11a4bbad4535cd822c180e8
Dyego Soriano <dyegodf2@gmail.com>
2020-12-25 11:20:03 -0300

    update route.items

--- src/routes/route.items.ts
+++ src/routes/route.items.ts
@@ -1,13 +1,32 @@
 import { Router } from 'express'
 
+import CreateItemService from '@services/CreateItemService'
+import DeleteItemService from '@services/DeleteItemService'
+
 import authUserMiddleware from '@middleware/authUser'
 
 const itemsRoute = Router()
 
 itemsRoute.use(authUserMiddleware)
 
-itemsRoute.post('/', async (request, response) => {})
-itemsRoute.put('/:id', async (request, response) => {})
-itemsRoute.delete('/:id', async (request, response) => {})
+itemsRoute.post('/', async (request, response) => {
+  const { amount, orderId, productId } = request.body
+  const owner = request.user.id
+
+  const createItem = new CreateItemService()
+  const item = await createItem.execute({ amount, orderId, productId, owner })
+
+  return response.json(item)
+})
+
+itemsRoute.delete('/:id', async (request, response) => {
+  const { id } = request.params
+  const { user } = request
+
+  const deleteItem = new DeleteItemService()
+  await deleteItem.execute({ id, user })
+
+  return response.json({ message: `O item foi deletado com sucesso!` })
+})
 
 export default itemsRoute
